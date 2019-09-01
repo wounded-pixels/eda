f70921a5c9060e90ee527c08fa4a0bacea7e9715
ctarrington <arrington.ct@gmail.com>
2019-08-31 20:51:26 -0400

    grids and background

--- project/src/lib/building-blocks/ScatterPlot.stories.ts
+++ project/src/lib/building-blocks/ScatterPlot.stories.ts
@@ -16,25 +16,33 @@ const data = [fred, barney, ted, wilma, betty, alice, tina];
 export const results: any = {};
 results.basic = createResizableDiv();
 results.zoomed = createResizableDiv();
-results.ticks = createResizableDiv();
+results.withTicks = createResizableDiv();
+results.seaborn = createResizableDiv();
 
 const basicPlot = new ScatterPlot(results.basic)
   .id(d => d.id)
   .domain(0, 100)
   .range(0, 200)
-  .fill(d => (d.gender === 'm' ? 'blue' : 'green'))
-  .opacity(0.5)
-  .radius(d => (d.gender === 'm' ? 3 : 5))
-  .stroke('blue')
-  .strokeWidth(0.2)
-  .axisWidth(3);
+  .xTicks([20, 40, 60, 80])
+  .yTicks([40, 80, 120, 160]);
 
 basicPlot.update(data);
-basicPlot
+basicPlot.position(d => d.height, d => d.weight);
+basicPlot.update(data);
+basicPlot.update(data);
+
+const tickedPlot = new ScatterPlot(results.withTicks)
+  .id(d => d.id)
+  .domain(0, 100)
+  .range(0, 200)
   .position(d => d.height, d => d.weight)
-  .fill(d => (d.gender === 'm' ? 'blue' : 'orange'));
-basicPlot.update(data);
-basicPlot.update(data);
+  .fill(d => (d.gender === 'm' ? 'blue' : 'orange'))
+  .axisStroke('black')
+  .axisStrokeWidth(3)
+  .tickStroke('blue')
+  .tickStrokeWidth(1)
+  .xTicks([20, 40, 60, 80])
+  .yTicks([40, 80, 120, 160]);
 
 const zoomedPlot = new ScatterPlot(results.zoomed)
   .id(d => d.id)
@@ -46,18 +54,30 @@ const zoomedPlot = new ScatterPlot(results.zoomed)
   .radius(d => (d.gender === 'm' ? 1 : 1.5))
   .stroke(d => (d.gender === 'm' ? 'black' : 'blue'))
   .strokeWidth(0.5)
-  .axisWidth(0.5);
-zoomedPlot.update(data);
+  .axisStrokeWidth(0.5)
+  .xTicks([50, 60, 70, 80])
+  .yTicks([120, 130, 140, 150]);
 
-const tickedPlot = new ScatterPlot(results.ticks)
+const seabornPlot = new ScatterPlot(results.seaborn)
   .id(d => d.id)
   .domain(0, 100)
   .range(0, 200)
   .position(d => d.height, d => d.weight)
-  .fill(d => (d.gender === 'm' ? 'blue' : 'orange'));
+  .fill(d => (d.gender === 'm' ? 'blue' : 'orange'))
+  .backgroundFill('lightgrey')
+  .axisStrokeWidth(2)
+  .axisStroke('none')
+  .tickStrokeWidth(1)
+  .tickStroke('white')
+  .xTicks([20, 40, 60, 80])
+  .yTicks([40, 80, 120, 160]);
+
+zoomedPlot.update(data);
 tickedPlot.update(data);
+seabornPlot.update(data);
 
 storiesOf('Scatter Plot', module)
   .add('basic construction', () => results.basic)
   .add('zoomed in', () => results.zoomed)
-  .add('basic with ticks', () => results.ticks);
+  .add('basic with ticks', () => results.withTicks)
+  .add('seaborn style', () => results.seaborn);
