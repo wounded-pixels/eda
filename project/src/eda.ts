3ea405c85524ba23f8178e73f3a963ee605869a4
ctarrington <arrington.ct@gmail.com>
2020-12-25 20:54:46 -0500

    fixed typings

--- project/src/eda.ts
+++ project/src/eda.ts
@@ -1,2 +1,4 @@
+export { DotPlot } from './lib/building-blocks/DotPlot';
 export { ScatterPlot } from './lib/building-blocks/ScatterPlot';
+export { SinaPlot } from './lib/building-blocks/SinaPlot';
 export { calculateDefaultTicks } from './lib/util/calculations';
